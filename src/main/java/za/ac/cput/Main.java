b60d898b4bb96b8c180ffc0135ccce8fc018f565
Thabiso Mbatha <thabisombatha204@gmail.com>
2025-05-11 15:19:37 +0200

    User Domain commit, All entities are protected

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -2,6 +2,7 @@ package za.ac.cput;
 
 import za.ac.cput.domain.Address;
 import za.ac.cput.domain.Contact;
+import za.ac.cput.domain.User;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class Main {
 
         Contact c = new Contact.Builder().setContactId(1).setEmail("test@test.com").build();
         System.out.println(c);
+
+        User u = new User.Builder().setUserId(2).setUserName("Thabiso").build();
+        System.out.println(u);
     }
 }
