60f09f3448a3564f29131a3a5e91334a8a185448
Siyabulela <S.Mgijima@plum.systems>
2025-08-31 13:41:54 +0200

    Controllers and Controller Tests

---
+++ src/test/java/za/ac/cput/controller/PlacementDataControllerTest.java
@@ -0,0 +1,43 @@
+/* PlacementDataControllerTest.java
+   Test class for PlacementDataController
+   Author: Siyabulela Mgijima (230680305)
+   Date: 30 August 2025
+*/
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+class PlacementDataControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findByPositionId() {
+    }
+
+    @Test
+    void findByRotationId() {
+    }
+
+    @Test
+    void findByScaleId() {
+    }
+}
---
+++ src/test/java/za/ac/cput/controller/PositionControllerTest.java
@@ -0,0 +1,43 @@
+/* PositionControllerTest.java
+   Test class for PositionController
+   Author: Siyabulela Mgijima (230680305)
+   Date: 30 August 2025
+*/
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+class PositionControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findByXCoordinate() {
+    }
+
+    @Test
+    void findByYCoordinate() {
+    }
+
+    @Test
+    void findByZCoordinate() {
+    }
+}
