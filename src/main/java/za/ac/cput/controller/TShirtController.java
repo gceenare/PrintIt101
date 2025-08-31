05114739cf963baa8720cd293fbfe056c6e1f419
Mcatshulwa <222614153@mycput.ac.za>
2025-08-31 20:57:02 +0200

    add TShirt, Design, and Product controllers with full CRUD operations

---
+++ src/main/java/za/ac/cput/controller/TShirtController.java
@@ -0,0 +1,60 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.TShirt;
+import za.ac.cput.service.TShirtService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tshirt")
+public class TShirtController {
+
+    private TShirtService service;
+
+    @Autowired
+    public TShirtController(TShirtService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/create")
+    public TShirt create(@RequestBody TShirt tshirt) {
+        return service.create(tshirt);
+    }
+
+    @GetMapping("/read/{id}")
+    public TShirt read(@PathVariable Integer id) {
+        return service.read(id);
+    }
+
+    @PutMapping("/update")
+    public TShirt update(@RequestBody TShirt tshirt) {
+        return service.update(tshirt);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable Integer id) {
+        return service.delete(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<TShirt> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/findBySize/{size}")
+    public List<TShirt> findBySize(@PathVariable String size) {
+        return service.findBySize(size);
+    }
+
+    @GetMapping("/findByColor/{color}")
+    public List<TShirt> findByColor(@PathVariable String color) {
+        return service.findByColor(color);
+    }
+
+    @GetMapping("/findByBrand/{brand}")
+    public List<TShirt> findByBrand(@PathVariable String brand) {
+        return service.findByBrand(brand);
+    }
+}
